60bc9fff24ec2111b3c93e78716249f35062d9fa
Oleksandr <oleksandr.starov@gmail.com>
2018-10-30 21:49:47 +0000

    or-115 - new endpoint for stats

--- backend/modules/serverCache.js
+++ backend/modules/serverCache.js
@@ -1,52 +1,53 @@
 "use strict";
 
 var cacheObject = {};
-module.exports.setData = function(prop, value){
-    if(!prop){
-        return;
-    }
-    cacheObject[prop] = value;
-    setRunnersStats();
+module.exports.setData = function (prop, value) {
+  if (!prop) {
+    return;
+  }
+  cacheObject[prop] = value;
+  setRunnersStats();
+  console.log('cache set');
 };
 
-module.exports.getData = function(prop){
-    if(!prop){
-        return null;
-    }
-    return cacheObject[prop];    
+module.exports.getData = function (prop) {
+  if (!prop) {
+    return null;
+  }
+  return cacheObject[prop];
 };
 
-function setRunnersStats(){
-    if(!cacheObject['runners'] || !cacheObject['statistics']){
-        return;
-    }
-    var allRunners = cacheObject['runners'].man.concat(cacheObject['runners'].woman);
-    
-    var places = allRunners.map(function(item){
-        if(item.PLACE_DIFF === null){
-            return 0;
-        }
-        return item.PLACE_DIFF;
-    });
-    
-   
-    var max =  Math.max.apply(null, places);
-    var min =  Math.min.apply(null, places);
-  
-    var progress = {up:[], down:[], novices:[]};
-    
-    
-    allRunners.forEach(function(item){
-       if(item.PLACE_DIFF === null){
-           progress.novices.push(item);
-       }
-       if(max && item.PLACE_DIFF == max){
-           progress.up.push(item);
-       }
-       if(min && item.PLACE_DIFF == min){
-           progress.down.push(item);
-       }
-    });
+function setRunnersStats() {
+  if (!cacheObject['runners'] || !cacheObject['statistics']) {
+    return;
+  }
+  var allRunners = cacheObject['runners'].man.concat(cacheObject['runners'].woman);
 
-    cacheObject['statistics'].progress = progress;
+  var places = allRunners.map(function (item) {
+    if (item.PLACE_DIFF === null) {
+      return 0;
+    }
+    return item.PLACE_DIFF;
+  });
+
+
+  var max = Math.max.apply(null, places);
+  var min = Math.min.apply(null, places);
+
+  var progress = {up: [], down: [], novices: []};
+
+
+  allRunners.forEach(function (item) {
+    if (item.PLACE_DIFF === null) {
+      progress.novices.push(item);
+    }
+    if (max && item.PLACE_DIFF == max) {
+      progress.up.push(item);
+    }
+    if (min && item.PLACE_DIFF == min) {
+      progress.down.push(item);
+    }
+  });
+
+  cacheObject['statistics'].progress = progress;
 }
